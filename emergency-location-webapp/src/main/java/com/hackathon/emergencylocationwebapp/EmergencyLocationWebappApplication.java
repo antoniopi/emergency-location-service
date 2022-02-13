f2f376dc69bc7abf69b54dbe853edce8bf701f6b
Antonio <jolly87@gmail.com>
2022-02-13 00:40:04 +0000

    add web application project

---
+++ emergency-location-webapp/src/main/java/com/hackathon/emergencylocationwebapp/EmergencyLocationWebappApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.emergencylocationwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyLocationWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyLocationWebappApplication.class, args);
+	}
+
+}
---
+++ emergency-location-webapp/src/test/java/com/hackathon/emergencylocationwebapp/EmergencyLocationWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.emergencylocationwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyLocationWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
