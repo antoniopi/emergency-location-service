407cda8855692a8ec3736981ec5af1c9fd92c077
Antonio Pizzurro <apizzurro@guidewire.com>
2022-02-16 11:49:28 +0000

    remove lombok from enum

--- emergency-location-webapp/src/main/java/com/hackathon/emergencylocationwebapp/model/EmergencyStatus.java
+++ emergency-location-webapp/src/main/java/com/hackathon/emergencylocationwebapp/model/EmergencyStatus.java
@@ -1,15 +1,10 @@
 package com.hackathon.emergencylocationwebapp.model;
 
-import lombok.*;
-
 import javax.persistence.Embeddable;
 
 @Embeddable
-@AllArgsConstructor
 public enum EmergencyStatus {
-    OPEN("Open"),
-    ONGOING("On going"),
-    CLOSED("Closed");
-
-    private String name;
+    OPEN,
+    ONGOING,
+    CLOSED;
 }
