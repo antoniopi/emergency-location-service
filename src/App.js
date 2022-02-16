9fdcd285b00585691f864faf16327998c944d97f
Daniel Connaughton <dconnaughton@guidewire.com>
2022-02-16 12:24:32 +0000

    Add React Leaflet

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {useState} from "react";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import './App.css';
+import { Icon } from "leaflet";
+// import * as parkData from "./data/skateboard-parks.json";
+
+function App() {
+  return (
+    <MapContainer center={[54.2, -6.34]} zoom={12}scrollWheelZoom={false}>
+      <TileLayer
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+    </MapContainer>
+  );
+}
+
+export default App;
